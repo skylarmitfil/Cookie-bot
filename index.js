f1744d91c4ea1894a57b03b224ecc2ee0bfbe031
Your Name <you@example.com>
2026-07-08 20:57:40 -0500

    Initial commit: Working Discord bot

---
+++ index.js
@@ -0,0 +1,4 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+client.once('ready', () => { console.log('ONLINE'); });
+client.login(process.env.DISCORD_TOKEN);

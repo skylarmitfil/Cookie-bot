3626475a713f24b06ce7f6e4314e44d578fd8a73
Skylar <skylarmitfil@gmail.com>
2026-07-15 21:54:36 -0500

    Create presence.js

---
+++ modules/presence.js
@@ -0,0 +1,24 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+    name: 'presence-manager',
+    init: (client) => {
+        // Double-check user is defined
+        if (!client.user) return; 
+
+        try {
+            client.user.setPresence({
+                activities: [{
+                    name: 'Watching Your OwO Commands',
+                    type: ActivityType.Streaming,
+                    url: 'https://www.youtube.com/watch?v=WaSy8yy-mr8'
+                }],
+                status: 'online'
+            });
+            console.log('[PRESENCE] Streaming status applied successfully!');
+        } catch (error) {
+            console.error('[PRESENCE ERROR]:', error);
+        }
+    },
+    execute: () => {}
+};

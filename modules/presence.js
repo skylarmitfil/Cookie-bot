e8ef2ddb13e8770bb5585cf9f778b0cabce85fed
Skylar <skylarmitfil@gmail.com>
2026-07-17 03:56:59 -0500

    Update presence.js

--- modules/presence.js
+++ modules/presence.js
@@ -1,21 +1,24 @@
-const { ActivityType } = require('discord.js');
+const { ActivityType, Events } = require('discord.js');
 
 module.exports = {
     name: 'presence-manager',
     init: (client) => {
-        try {
-            client.user.setPresence({
-                activities: [{
-                    name: 'Watching Your OwO Commands',
-                    type: ActivityType.Streaming,
-                    url: 'https://www.youtube.com/watch?v=WaSy8yy-mr8'
-                }],
-                status: 'online'
-            });
-            console.log('[PRESENCE] Streaming status applied successfully!');
-        } catch (error) {
-            console.error('[PRESENCE ERROR]:', error);
-        }
+        // We wait for the client to be ready before setting the presence
+        client.once(Events.ClientReady, () => {
+            try {
+                client.user.setPresence({
+                    activities: [{
+                        name: 'Watching Your OwO Commands',
+                        type: ActivityType.Watching, // Changed to Watching
+                        // URL only works for Streaming; for others, it's not needed
+                    }],
+                    status: 'online'
+                });
+                console.log('[PRESENCE] Status set successfully!');
+            } catch (error) {
+                console.error('[PRESENCE ERROR]:', error);
+            }
+        });
     },
     execute: () => {}
 };

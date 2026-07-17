d315581e6c0863de5a25ecb0f75a5cee5131eafd
Skylar <skylarmitfil@gmail.com>
2026-07-17 04:11:29 -0500

    Update presence.js

--- modules/presence.js
+++ modules/presence.js
@@ -10,9 +10,7 @@ module.exports = {
                         name: 'Watching Your OwO Commands',
                         type: ActivityType.Streaming, 
                         // You MUST use a valid twitch.tv URL for the 'Watch' button to appear
-                        url: 'https://m.youtube.com/watch?v=h7oSlZL0tEM&list=RDh7oSlZL0tEM&start_radio=1' 
-                    }],
-                    status: 'online'
+                        url: 'https://m.twitch.tv/skylar_mitifil/home'
                 });
                 console.log('[PRESENCE] Streaming status applied!');
             } catch (error) {
